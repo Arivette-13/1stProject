378a87005c2bb86b3893bc11d64531b52707ca75
Arivette-13 <arivette.sp@gmail.com>
2024-07-26 16:06:44 -0600

    Update Proyecto 1.py

--- Proyecto 1.py
+++ Proyecto 1.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
 from Cmd import gen_pass
+import os
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix = '!', intents=intents)
 
-token = 'MWAFhuu1937287398DWFDMrefhehj1738'
+    token = 'IEWHFU238422IUH3DSJADN2393201931EYGYF12D1Y239Y1H2UBG'
 
 @bot.event
 async def on_read():
@@ -24,5 +26,38 @@ async def test(ctx, arg):
 async def test2(ctx, *args):
     arguments = ', '.join(args)
     await ctx.send(f'{len(args)} arguments: {arguments}')
+
+@bot.command(name = 'meme')
+async def memes(ctx):
+    with open('imagenes/meme1.jpg', 'rb') as g:
+        picture = discord.File(g)
+    await ctx.send(file = picture)
+
+@bot.command(name = 'memes')
+async def memes(ctx):
+    img_mem = random.choice(os.listdir('imagenes'))
+    with open(f'imagenes/{img_mem}', 'rb') as g:
+        picture = discord.File(g)
+    await ctx.send(file = picture)
+
+#NEW
+@bot.command(name = 'play')
+async def juegos(ctx):
+    img_SudoF = random.choice(os.listdir('SF'))
+    img_SudoN = random.choice(os.listdir('SN'))
+    img_SudoD = random.choice(os.listdir('SD'))
+   #Sudoku facil
+    with open(f'SF/{img_SudoF}', 'rb') as f:
+        izi = discord.File(f)
+    await ctx.send(file = izi)
+    #sudoku medio
+    with open(f'SN/{img_SudoN}', 'rb') as m:
+        mid = discord.File(m)
+    await ctx.send(file = mid)
+    #sudoku dificil
+    with open(f'SD/{img_SudoD}', 'rb') as d:
+        hard = discord.File(d)
+    await ctx.send(file = hard)
+#/NEW
 
 bot.run(token)
